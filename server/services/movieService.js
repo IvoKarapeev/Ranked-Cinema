532728a16825698e2b46b757088a81bb3bdab3bc
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 16:29:26 +0300

    creating service to get one movie

--- server/services/movieService.js
+++ server/services/movieService.js
@@ -3,3 +3,5 @@ const Movie = require('../models/Movie');
 exports.getAll = () => Movie.find();
 
 exports.create = (movieItam) => Movie.create(movieItam);
+
+exports.getOneDetailed = (movieId) => Movie.findById(movieId);

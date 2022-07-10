a67b7bfa6efcb52d5dcf17c300a36fafa87eaab6
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 15:43:00 +0300

    importing cors function in index

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { PORT } = require('./config/env');
 const { dbInit } = require('./config/db');
+const cors = require('./middlewares/cors');
 
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.urlencoded({extended : false}));
+app.use(cors());
 app.use(routes);
 
 dbInit();

2b38e48b8a368083556b0db9b5319d72c14b3b12
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 11:38:19 +0300

    update db_quearystring

--- server/config/env.js
+++ server/config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3000; 
-exports.DB_QUEARYSTRING = 'mongodb+srv://Ivo:CodingIsGood123@ivocorp.meizoti.mongodb.net/test'; 
+exports.DB_QUEARYSTRING = 'mongodb+srv://Ivo:CodingIsGood123@ivocorp.meizoti.mongodb.net/Movies'; 
 exports.SALT_ROUNDS = 10;
 exports.SECRET = '213njdu2nsdiaj821njd82';

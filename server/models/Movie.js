d3f7d8766ce677d7168748d541c67a8e9635becb
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 11:11:59 +0300

    updating movie schema

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,7 +1,34 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-    
+    name:{
+        type: String,
+        required:true,
+    },
+    description:{
+        type: String,
+        required:true,
+    },
+    imageUrl:{
+        type: String,
+        required:true,
+    },
+    actors:{
+        type: String,
+        required:true,
+    },
+    author:{
+        type: String,
+        required:true,  
+    },
+    likes:[{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
+    creator:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 const Movie = mongoose.model('Movie',movieSchema);

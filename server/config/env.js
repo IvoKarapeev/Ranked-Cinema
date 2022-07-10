ae6ea854ccf7bd99e4c80954230b452739df10e4
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 10:56:14 +0300

    exporting configurations

---
+++ server/config/env.js
@@ -0,0 +1,4 @@
+exports.PORT = 3000; 
+exports.DB_QUEARYSTRING = 'mongodb+srv://Ivo:CodingIsGood123@ivocorp.meizoti.mongodb.net/test'; 
+exports.SALT_ROUNDS = 10;
+exports.SECRET = '213njdu2nsdiaj821njd82';

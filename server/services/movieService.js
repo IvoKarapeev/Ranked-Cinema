b7a4e4031bace5a29a408fd93212cc7a41cedf83
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 18:15:07 +0300

    making service for deleting movie

--- server/services/movieService.js
+++ server/services/movieService.js
@@ -10,3 +10,5 @@ exports.getOne = (movieId) => Movie.findById(movieId);
 
 exports.update = (movieId,movieData) => Movie.findByIdAndUpdate(movieId,movieData);
 
+exports.delete = (movieId) => Movie.findByIdAndDelete(movieId);
+

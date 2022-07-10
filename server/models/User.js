5999ba04ec4c14cfc3ae86f1e08f953bc7cf230e
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-10 11:05:12 +0300

    update user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    
+    firstName:{
+        type: String,
+        required:true,
+    },
+    secondName:{
+        type: String,
+        required:true,
+    },
+    username:{
+        type: String,
+        required:true,
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    likedMovies: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }]
 });
 
 const User = mongoose.model('User',userSchema);

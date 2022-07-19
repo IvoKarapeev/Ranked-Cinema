36add2f0c8cf98cf71020d80d4df5c3cfee52ef3
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-19 19:03:22 +0300

    making route for register user

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const userService = require('../services/userService');
+
+
+router.post('/register',async (req,res) => {
+
+    const { firstName, secondName, username, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+        return res.json('Passwords must be equal!');
+    };
+
+    try {
+        
+        const user = await userService.register(firstName, secondName, username, password);
+        const token = await userService.createToken(user);
+
+        return {
+            username:user.username,
+            token,
+            _id:username._id
+        };
+
+    } catch (error) {
+
+        console.log(error);
+    }
+
+});
+
+module.exports = router
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const movieController = require('./controllers/movieController');
+const userController = require('./controllers/userController');
 
-router.use('/movies',movieController)
+router.use('/movies',movieController);
+router.use('/user',userController);
 
 module.exports = router;

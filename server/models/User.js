cfddb6203598510f83489b1f4d3e6f47f9c99c47
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-20 10:37:10 +0300

    Validation for creating user

--- server/models/User.js
+++ server/models/User.js
@@ -4,14 +4,17 @@ const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
         required:true,
+        minlength:[2,'The firstName should be at least 2 characters long']
     },
     secondName:{
         type: String,
         required:true,
+        minlength:[2,'The secondName should be at least 2 characters long']
     },
     username:{
         type: String,
         required:true,
+        minlength:[5,'The username should be at least 5 characters long']
     },
     password: {
         type: String,

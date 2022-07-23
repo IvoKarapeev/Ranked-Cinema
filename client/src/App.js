6cb5fa7e3aca01802d47f35332baa24a3015f6f5
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-23 10:11:28 +0300

    Creating home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+
 
 function App() {
     return (
         <div>
             
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+            </main>
         </div>
     );
 }

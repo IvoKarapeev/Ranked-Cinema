b941b38d15e2726b8860b884c4e192748f96ff0e
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-23 14:13:46 +0300

    Finishing home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,7 +3,14 @@ import styles from './Home.module.css';
 const Home = () => {
     return (
         <section className={styles['home-background']}>
-            
+            <div className={styles['home-information']}>
+                Welcome to Ranked-Cinema! Here you can find all you're favorite
+                movies! You can comment and share with us what was you're experience when 
+                you saw them for the first time. Or you can add you're favorite movies if they
+                are not in our catalog of movies. Also you can see which are the most 
+                popular movies now in our ranking page. Have fun and we hope you like this
+                page! 
+            </div>
         </section>
     );
 };

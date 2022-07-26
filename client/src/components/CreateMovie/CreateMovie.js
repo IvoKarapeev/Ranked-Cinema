4964c61e65e11ff44013e750760cd724a28d02e0
DarkHouse123 <ivokarapeev@gmail.com>
2022-07-26 11:55:41 +0300

    Making Create Movie page Validation
    And making the form controlled

--- client/src/components/CreateMovie/CreateMovie.js
+++ client/src/components/CreateMovie/CreateMovie.js
@@ -1,12 +1,63 @@
+import { useState } from 'react';
 import styles from './CreateMovie.module.css';
 
 const CreateMovie = () => {
+
+    const [gameData,setGameData] = useState({
+        name:'',
+        description:'',
+        imageUrl:'',
+        trailerUrl:'',
+        actors:'',
+        category:'',
+        author:''
+    });
+
+    const [errors,setErrors] = useState({});
+
+
+    const onChange = (e) => {
+        setGameData(state => ({
+            ...state,
+            [e.target.name]:e.target.value
+        }));
+    };
+
+    const minlength = (e,limit) => {
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: gameData[e.target.name].length < limit,
+        }));
+    };
+
+    const httpValidation = (e) => {
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: !gameData[e.target.name].startsWith('http')
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(gameData);
+        setGameData({
+            name:'',
+            description:'',
+            imageUrl:'',
+            trailerUrl:'',
+            actors:'',
+            category:'',
+            author:''
+        });
+    };
+
     return (
-        <form action="action_page.php">
+        <form onSubmit={onSubmit}>
             <div className={styles.container}>
-            <h1>Create Game</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr />
+            <h1>Create Movie</h1>
+            <p>Please fill in this form to create movie.</p>
+            <hr className={styles.separator}/>
             <label htmlFor="movieName">
                 <b>Movie Name</b>
             </label>
@@ -14,9 +65,15 @@ const CreateMovie = () => {
                 className={styles["input-text"]}
                 type="text"
                 placeholder="Enter Movie Name"
-                name="firstName"
-                id="firstName"
+                name="name"
+                id="name"
+                value={gameData.name}
+                onChange={onChange}
+                onBlur={(e) => minlength(e,2)}
             />
+            {errors.name &&
+                <div className={styles.validate}>Name should be at least 2 characters long</div>
+            }
              <label htmlFor="description">
                 <b>Description</b>
             </label>
@@ -26,7 +83,13 @@ const CreateMovie = () => {
                 placeholder="Description"
                 name="description"
                 id="description"
+                value={gameData.description}
+                onChange={onChange}
+                onBlur={(e) => minlength(e,10)}
             />
+            {errors.description &&
+                <div className={styles.validate}>Description should be at least 10 characters long</div>
+            }
              <label htmlFor="imageUrl">
                 <b>Movie Image</b>
             </label>
@@ -36,7 +99,13 @@ const CreateMovie = () => {
                 placeholder="Enter imageUrl"
                 name="imageUrl"
                 id="imageUrl"
+                value={gameData.imageUrl}
+                onChange={onChange}
+                onBlur={(e) => httpValidation(e)}
             />
+            {errors.imageUrl &&
+                <div className={styles.validate}>imageUrl should starts with http/https</div>
+            }
             <label htmlFor="trailerUrl">
                 <b>Movie Trailer</b>
             </label>
@@ -46,7 +115,13 @@ const CreateMovie = () => {
                 placeholder="Enter trailerUrl"
                 name="trailerUrl"
                 id="trailerUrl"
+                value={gameData.trailerUrl}
+                onChange={onChange}
+                onBlur={(e) => httpValidation(e)}
             />
+            {errors.trailerUrl &&
+                <div className={styles.validate}>trailerUrl should starts with http/https</div>
+            }
             <label htmlFor="actors">
                 <b>Actors</b>
             </label>
@@ -56,6 +131,8 @@ const CreateMovie = () => {
                 placeholder="Enter Actors"
                 name="actors"
                 id="actors"
+                value={gameData.actors}
+                onChange={onChange}
             />
              <label htmlFor="category">
                 <b>Category</b>
@@ -66,6 +143,8 @@ const CreateMovie = () => {
                 placeholder="Enter Category"
                 name="category"
                 id="category"
+                value={gameData.category}
+                onChange={onChange}
             />
             <label htmlFor="author">
                 <b>Author</b>
@@ -76,10 +155,12 @@ const CreateMovie = () => {
                 placeholder="Enter Moive Author"
                 name="author"
                 id="author"
+                value={gameData.author}
+                onChange={onChange}
             />
-            <hr/>
+            <hr className={styles.separator}/>
             <button type="submit" className={styles.createbtn}>
-                Create Game
+                Create Movie
             </button>
             </div>
         </form>
